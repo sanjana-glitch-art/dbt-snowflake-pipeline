b761313190e9e8ace80b84fefbe7034a11894871
Thummalapalli Sai Teja Sri Sanjana <tsaitejasrisanjana@gmail.com>
2026-04-16 01:53:43 -0700

    Add snapshot_session_summary SQL table definition
    Create a transient table for session summary with necessary fields.

---
+++ target/run/build_mau/snapshots/snapshot_session_summary.sql
@@ -0,0 +1,38 @@
+
+      
+  
+    
+
+create or replace transient table USER_DB_FLAMINGO.snapshot.snapshot_session_summary
+    
+    
+    
+    as (
+    
+
+    select *,
+        md5(coalesce(cast(sessionId as varchar ), '')
+         || '|' || coalesce(cast(ts as varchar ), '')
+        ) as dbt_scd_id,
+        ts as dbt_updated_at,
+        ts as dbt_valid_from,
+        
+  
+  coalesce(nullif(ts, ts), null)
+  as dbt_valid_to
+from (
+        
+
+
+SELECT * FROM USER_DB_FLAMINGO.ANALYTICS.session_summary
+
+    ) sbq
+
+
+
+    )
+;
+
+
+  
+  
